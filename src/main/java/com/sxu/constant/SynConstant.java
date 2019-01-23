aedcc0d2d216fa1f0fb2c49c1934f91a90761b27
1303575952@qq.com <1303575952@qq.com>
2019-01-23 09:59:33 +0800

    nettyserver-heartbeat

--- src/main/java/com/sxu/constant/SynConstant.java
+++
@@ -1,6 +0,0 @@
-package com.sxu.constant;
-
-public class SynConstant {
-    //当前是否同步，初始为同步
-    public static boolean isSyn = true;
-}
--- src/main/java/com/sxu/nettyserver/NettyserverApplication.java
+++ src/main/java/com/sxu/nettyserver/NettyserverApplication.java
@@ -1,21 +1,14 @@
 package com.sxu.nettyserver;
 
-import com.sxu.server.EchoServer;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NettyserverApplication implements CommandLineRunner {
+public class NettyserverApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(NettyserverApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        new EchoServer(8888).start();
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(NettyserverApplication.class, args);
+	}
 
 }
 
