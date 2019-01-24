2b2339f81f1985ea4f615153aed89fcce41d9f8a
1303575952@qq.com <1303575952@qq.com>
2019-01-24 10:45:23 +0800

    nettyserver-heartbeat 修改server启动方式

--- src/main/java/com/sxu/NettyserverApplication.java
+++ src/main/java/com/sxu/NettyserverApplication.java
@@ -1,14 +1,20 @@
 package com.sxu;
 
+import com.sxu.server.Server;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NettyserverApplication {
+public class NettyserverApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NettyserverApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		new Server(8081).start();
+	}
 }
 
--- src/main/java/com/sxu/server/Server.java
+++ src/main/java/com/sxu/server/Server.java
@@ -7,8 +7,20 @@ import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.timeout.IdleStateHandler;
+
 public class Server {
-    public static void main(String[] args) {
+    private final int port;
+
+    public Server(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        new Server(8081).start();
+    }
+
+    public void start() throws Exception {
         EventLoopGroup bossGroup = new NioEventLoopGroup(1);
 
         EventLoopGroup workerGroup = new NioEventLoopGroup(4);
@@ -16,27 +28,27 @@ public class Server {
             ServerBootstrap serverBootstrap = new ServerBootstrap();
 
             serverBootstrap.group(bossGroup, workerGroup)
-            .channel(NioServerSocketChannel.class)
-            .localAddress(8081)
-            .childHandler(new ChannelInitializer<Channel>() {
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(port)
+                    .childHandler(new ChannelInitializer<Channel>() {
 
-                @Override
-                protected void initChannel(Channel ch) throws Exception {
-                    // TODO Auto-generated method stub
-                    ChannelPipeline pipeline = ch.pipeline();
-                    pipeline.addLast(new IdleStateHandler(10,0,0));
-                    pipeline.addLast(new MsgPckDecode());
-                    pipeline.addLast(new MsgPckEncode());
-                    pipeline.addLast(new server3Handler());
-                }
-            });         
-            System.out.println("start Server 8081 --");
+                        @Override
+                        protected void initChannel(Channel ch) throws Exception {
+                            // TODO Auto-generated method stub
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast(new IdleStateHandler(10, 0, 0));
+                            pipeline.addLast(new MsgPckDecode());
+                            pipeline.addLast(new MsgPckEncode());
+                            pipeline.addLast(new server3Handler());
+                        }
+                    });
+            System.out.println("start Server" + port + "--");
             ChannelFuture sync = serverBootstrap.bind().sync();
             sync.channel().closeFuture().sync();
         } catch (InterruptedException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-        }finally{
+        } finally {
             //优雅的关闭资源
             bossGroup.shutdownGracefully();
             workerGroup.shutdownGracefully();
