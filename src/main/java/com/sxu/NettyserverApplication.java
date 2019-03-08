25d8c25d476d9266051fee610583a75a094b5e1c
1303575952@qq.com <1303575952@qq.com>
2019-03-08 22:45:16 +0800

    nettyserver

--- src/main/java/com/sxu/NettyserverApplication.java
+++ src/main/java/com/sxu/NettyserverApplication.java
@@ -1,20 +1,29 @@
 package com.sxu;
 
-import com.sxu.server.Server;
+import com.sxu.server.EchoServer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NettyserverApplication implements CommandLineRunner {
+public class NettyserverApplication extends SpringBootServletInitializer implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NettyserverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NettyserverApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		new Server(8081).start();
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        new EchoServer(8085).start();
+    }
+
+    //为了打包springboot项目
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
+
 }
 
--- src/main/java/com/sxu/config/JDBCConfiguration.java
+++ src/main/java/com/sxu/config/JDBCConfiguration.java
@@ -4,11 +4,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class JDBCConfiguration {
-    public static String HOST = "127.0.0.1";
+    public static String HOST = "39.96.33.44";
     public static String PORT = "3306";
     public static String DATABASE_NAME = "received_usr_info";
     public static String USER_NAME = "root";
-    public static String PASSWORD = "123456";
+    public static String PASSWORD = "root";
 
     /**
      * 获取数据库连接
